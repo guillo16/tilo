c0bbc8b05a5c65f9e507f3c92d1ee660027d5dab
guillermo varela <guillovarela16@gmail.com>
2020-06-05 10:00:51 -0300

    Create product controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'products/index'
+  get 'products/show'
+  get 'products/new'
+  get 'products/create'
+  get 'products/edit'
+  get 'products/update'
+  get 'products/destroy'
   get 'categories/show'
   get 'categories/new'
   get 'categories/create'

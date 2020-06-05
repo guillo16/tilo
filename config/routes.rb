6204cbb0b9f5a13f37c25fddfbafedf9bd0cfa1f
guillermo varela <guillovarela16@gmail.com>
2020-06-05 10:31:11 -0300

    Create new files

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :divisions, except: [:index, :destroy]
-  resources :categories, except: :index
+  resources :divisions, except: :destroy
+  resources :categories, except: :destroy
   resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

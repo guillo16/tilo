a91e27c4eeb4eb891688a9c99f181d4c5bdc69c4
guillermo varela <guillovarela16@gmail.com>
2020-06-05 09:59:59 -0300

    Product created

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_125611) do
+ActiveRecord::Schema.define(version: 2020_06_05_125935) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,19 @@ ActiveRecord::Schema.define(version: 2020_06_05_125611) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "products", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "photo"
+    t.integer "stock"
+    t.bigint "category_id"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+    t.index ["user_id"], name: "index_products_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -43,4 +56,6 @@ ActiveRecord::Schema.define(version: 2020_06_05_125611) do
   end
 
   add_foreign_key "categories", "divisions"
+  add_foreign_key "products", "categories"
+  add_foreign_key "products", "users"
 end

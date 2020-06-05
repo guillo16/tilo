1e078dcdda94d9fc8d3e5afcd8d624ff0b6125fb
guillermo varela <guillovarela16@gmail.com>
2020-06-05 09:56:40 -0300

    Create category controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_05_125434) do
+ActiveRecord::Schema.define(version: 2020_06_05_125611) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "title"
+    t.bigint "division_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["division_id"], name: "index_categories_on_division_id"
+  end
 
   create_table "divisions", force: :cascade do |t|
     t.string "title"
@@ -34,4 +42,5 @@ ActiveRecord::Schema.define(version: 2020_06_05_125434) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "categories", "divisions"
 end

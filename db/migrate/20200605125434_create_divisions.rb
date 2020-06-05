9a873b6ed468f3a9f545d97c0debcddb6543b3bc
guillermo varela <guillovarela16@gmail.com>
2020-06-05 09:55:05 -0300

    Create division model

---
+++ db/migrate/20200605125434_create_divisions.rb
@@ -0,0 +1,10 @@
+class CreateDivisions < ActiveRecord::Migration[5.2]
+  def change
+    create_table :divisions do |t|
+      t.string :title
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end

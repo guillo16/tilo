704597b4a35778262960acd49a092170a799f32e
guillermo varela <guillovarela16@gmail.com>
2020-06-05 10:06:15 -0300

    Finish routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,22 +1,9 @@
 Rails.application.routes.draw do
-  get 'products/index'
-  get 'products/show'
-  get 'products/new'
-  get 'products/create'
-  get 'products/edit'
-  get 'products/update'
-  get 'products/destroy'
-  get 'categories/show'
-  get 'categories/new'
-  get 'categories/create'
-  get 'categories/edit'
-  get 'categories/update'
-  get 'divisions/show'
-  get 'divisions/new'
-  get 'divisions/create'
-  get 'divisions/edit'
-  get 'divisions/update'
   devise_for :users
   root to: 'pages#home'
+
+  resources :divisions, except: [:index, :destroy]
+  resources :categories, except: :index
+  resources :products
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

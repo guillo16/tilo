c0a8beb1d51ebf5023ddd2d644d60d6d3a722b34
guillermo varela <guillovarela16@gmail.com>
2020-06-09 09:41:25 -0300

    Create new folders

--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,9 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show]
   resources :line_items
   resources :carts
+
+  resources :orders, only: [:index, :show, :create] do
+    resources :payments, only: :new
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

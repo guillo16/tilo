74fd388914a3910815f6799448d6bf91c5a83229
guillermo varela <guillovarela16@gmail.com>
2020-06-09 09:23:03 -0300

    Add price to products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_08_200259) do
+ActiveRecord::Schema.define(version: 2020_06_09_122206) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 2020_06_08_200259) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "flavor"
+    t.integer "price_cents", default: 0, null: false
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end

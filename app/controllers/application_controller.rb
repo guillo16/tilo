7bd8c38181e2b1fd5a6b4c40cfa54c9d5e477e45
guillermo varela <guillovarela16@gmail.com>
2020-06-09 14:47:11 -0300

    Finish style forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   before_action :set_divisions
   after_action :store_action
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 

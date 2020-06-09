dbdefec4ef5b430b965e6a4f43912e455f1fd2f2
guillermo varela <guillovarela16@gmail.com>
2020-06-09 10:00:11 -0300

    Finish the payment new style

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -1,2 +1,20 @@
 class PaymentsController < ApplicationController
+  before_action :set_order
+  def new
+  end
+
+  def create
+    # DESTROY CART
+    @cart = Cart.find(session[:cart_id])
+    session[:cart_id] = nil
+    @order.update(state: 'Encargado')
+    # OrderMailer.with(order: @order).new_order_email.deliver_later
+    redirect_to order_path(@order)
+  end
+
+  private
+
+  def set_order
+    @order = current_user.orders.where(state: 'Pendientes').find(params[:order_id])
+  end
 end

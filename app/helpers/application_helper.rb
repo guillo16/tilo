5a5f047edb42508fcf03eca7cdd5f21d1f8d6103
guillermo varela <guillovarela16@gmail.com>
2020-06-09 17:00:53 -0300

    Finish navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,11 @@
 module ApplicationHelper
+
   def cart_count_over_one
-    if @cart.line_items.count > 0
-      return "<span class='tag is-dark'>#{@cart.line_items.count}</span>".html_safe
-    end
+    return total_cart_items if total_cart_items > 0
   end
 
-  def cart_has_items
-    return @cart.line_items.count > 0
+  def total_cart_items
+    total = @cart.line_items.map(&:quantity).sum
+    return total if total > 0
   end
 end

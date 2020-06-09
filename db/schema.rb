a28d4e0176aa7ba6d47d8d7dc0cb7e38a41fa74e
guillermo varela <guillovarela16@gmail.com>
2020-06-09 14:36:28 -0300

    Finish migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_123039) do
+ActiveRecord::Schema.define(version: 2020_06_09_173516) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -103,6 +103,10 @@ ActiveRecord::Schema.define(version: 2020_06_09_123039) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "admin", default: false, null: false
+    t.string "name"
+    t.string "address"
+    t.string "phone"
+    t.string "city"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

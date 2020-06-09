800a6ab11668c6f5fdecd013463010ff9468d51c
guillermo varela <guillovarela16@gmail.com>
2020-06-09 09:36:32 -0300

    New file added

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,21 @@
 class OrdersController < ApplicationController
+  def index
+    if current_user.admin?
+      @order = Order.sum('amount_cents') * 0.01
+      @orders = Order.order(created_at: :desc)
+    else
+      redirect_to root_path
+      flash[:notice] = "Accesso denegado!"
+    end
+  end
+
+  def show
+    @order = current_user.orders.where(state: 'Encargado').find(params[:id])
+  end
+
+  def create
+    cart = Cart.find(params[:cart_id])
+    order = Order.create!(amount: cart.total_price, state: 'Pendientes', user: current_user, cart: cart)
+    redirect_to new_order_payment_path(order)
+  end
 end

247990d44ac88fe2597742589a6136873a266912
guillermo varela <guillovarela16@gmail.com>
2020-06-10 10:23:29 -0300

    Destroy payment

--- test/mailers/previews/payment_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# Preview all emails at http://localhost:3000/rails/mailers/payment_mailer
-class PaymentMailerPreview < ActionMailer::Preview
-
-  # Preview this email at http://localhost:3000/rails/mailers/payment_mailer/new_payment
-  def new_payment
-    PaymentMailer.new_payment
-  end
-
-end

a4a7dd5dcab04c4593908faf52bcf3276693037d
guillermo varela <guillovarela16@gmail.com>
2020-06-10 11:48:07 -0300

    Finish seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,18 +10,26 @@
 
 puts 'clearing database'
 
+Order.delete_all
+LineItem.delete_all
+Cart.delete_all
 Product.delete_all
 Category.delete_all
 Division.delete_all
+User.delete_all
+
+puts 'creating user'
+User.create(email: 'user1@hotmail.com', password: 123456, admin: true)
+
 
 puts 'Creating division'
 
 alacena = Division.create(title: 'Alacena', photo: 'https://res.cloudinary.com/dokddymx6/image/upload/v1591538997/Captura_de_Pantalla_2020-06-07_a_la_s_11.07.13_k1pgqd.png')
-bebidas = Division.create(title: 'Bebidas', photo: 'https://images.unsplash.com/photo-1528556860752-2a6a19a285a3?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=932&q=80')
-snacks = Division.create(title: 'Snacks', photo: 'https://images.unsplash.com/photo-1566478989037-eec170784d0b?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
-cosmetica = Division.create(title: 'Cosmetica natural', photo: 'https://res.cloudinary.com/dokddymx6/image/upload/v1591538999/Captura_de_Pantalla_2020-06-07_a_la_s_11.08.17_ypkxd8.png')
+bebidas = Division.create(title: 'Bebidas', photo: 'https://res.cloudinary.com/dokddymx6/image/upload/v1591799182/Captura_de_Pantalla_2020-06-10_a_la_s_11.19.19_w99wgc.jpg')
+snacks = Division.create(title: 'Snacks', photo: 'https://res.cloudinary.com/dokddymx6/image/upload/c_scale,w_1346/v1591799161/Captura_de_Pantalla_2020-06-10_a_la_s_11.23.24_wgiddd.jpg')
+cosmetica = Division.create(title: 'Cosmetica natural', photo: 'https://res.cloudinary.com/dokddymx6/image/upload/v1591799081/1561_1_pd6bi3.jpg')
 heladera = Division.create(title: 'Heladera', photo: 'https://images.unsplash.com/photo-1501443762994-82bd5dace89a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80')
-suplementos = Division.create(title: 'Suplementos', photo: 'https://images.unsplash.com/photo-1579722821273-0f6c7d44362f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80')
+suplementos = Division.create(title: 'Suplementos', photo: 'https://res.cloudinary.com/dokddymx6/image/upload/c_scale,w_1477/v1591799186/Captura_de_Pantalla_2020-06-10_a_la_s_11.17.48_jxhy3j.jpg')
 
 puts 'creating categories'
 

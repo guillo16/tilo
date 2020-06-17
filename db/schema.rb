8c9319ed7836231a5767fa43ab44377e62ecd827
guillermo varela <guillovarela16@gmail.com>
2020-06-17 09:03:28 -0300

    Edit params from products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_173516) do
+ActiveRecord::Schema.define(version: 2020_06_17_115912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -90,6 +90,8 @@ ActiveRecord::Schema.define(version: 2020_06_09_173516) do
     t.datetime "updated_at", null: false
     t.string "flavor"
     t.integer "price_cents", default: 0, null: false
+    t.string "brand"
+    t.string "condition"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end

68b9af4afcfc15ead38d4a535ce5e9071069de16
guillermo varela <guillovarela16@gmail.com>
2020-06-17 14:46:16 -0300

    Add sale to product

---
+++ db/migrate/20200617174554_add_sale_to_product.rb
@@ -0,0 +1,5 @@
+class AddSaleToProduct < ActiveRecord::Migration[5.2]
+  def change
+    add_column :products, :sale, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_17_174407) do
+ActiveRecord::Schema.define(version: 2020_06_17_174554) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -94,6 +94,7 @@ ActiveRecord::Schema.define(version: 2020_06_17_174407) do
     t.string "condition"
     t.string "vegan"
     t.string "sugar"
+    t.string "sale"
     t.index ["category_id"], name: "index_products_on_category_id"
     t.index ["user_id"], name: "index_products_on_user_id"
   end

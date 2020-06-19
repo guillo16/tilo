bcdbafe9e7d25e4c5eaff6c9258d82e80c36626f
guillermo varela <guillovarela16@gmail.com>
2020-06-19 10:37:46 -0300

    Add forma de pago in views and controller

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -5,9 +5,10 @@ class PaymentsController < ApplicationController
 
   def create
     # DESTROY CART
+    payment = params[:payment_method]
     @cart = Cart.find(session[:cart_id])
     session[:cart_id] = nil
-    @order.update(state: 'Encargado')
+    @order.update(state: 'Encargado', method: payment)
     OrderMailer.with(order: @order).new_order.deliver_later
     OrderMailer.with(order: @order).new_payment.deliver_later
     redirect_to order_path(@order)

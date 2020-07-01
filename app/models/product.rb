8ba1bf22d55572ff53d6496935de03a59e3e43cd
guillermo varela <guillovarela16@gmail.com>
2020-07-01 19:52:57 -0300

    Add style

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,10 +3,10 @@ class Product < ApplicationRecord
   belongs_to :user
   has_many :line_items, dependent: :destroy
   monetize :price_cents
+  paginates_per 6
 
   validates :title, presence: true
   validates :description, presence: true
   validates :stock, presence: true
   has_many_attached :photos
-
 end

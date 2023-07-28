abf8a7b90566bbc10c7da39886bab58bfe50bc53
guillermo varela <guillovarela16@gmail.com>
2023-07-28 17:09:32 -0300

    Change somethings

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,7 +2,7 @@ class Category < ApplicationRecord
   extend FriendlyId
   friendly_id :title, use: :slugged
   belongs_to :division
-  has_many :products
+  has_many :products, dependent: :destroy
 
   validates :title, presence: true
 end
